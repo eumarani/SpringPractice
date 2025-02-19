9fe91b4e1bf84c2a4fa674db4bff90b9e1004098
Umarani <eumarani456@gmail.com>
2025-02-19 12:20:35 +0530

    Updated EmployeeManagementApplication.java

--- employee-management/src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ employee-management/src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -1,7 +1,7 @@
 package com.example.employee_management;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;git
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
